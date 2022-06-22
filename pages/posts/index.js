20426e4ab66861b1371f23c7e0e123f7462fdb9c
mkovalev <marina.kovaleva@t-systems.com>
2022-06-22 15:25:12 +0300

    Initial commit

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Link from 'next/link';
-import Date from '../components/date';
-import utilStyles from '../styles/utils.module.css';
-import Layout, { siteTitle } from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../../components/layout';
+import utilStyles from '../../styles/utils.module.css';
+import { getSortedPostsData } from '../../lib/posts';
 
-export default function Home({ allPostsData }) {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -13,13 +20,17 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              {title}
               <br />
               {id}
               <br />
@@ -29,14 +40,5 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+  );
 }
